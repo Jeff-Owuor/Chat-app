fa84a5321526ed0c559fd9451484f4b894009adf
moni-que <monicahjustus@gmail.com>
2022-06-22 10:38:42 +0300

    create signup and login views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegisterForm
+from django.contrib.auth.models import auth
+from django.contrib import messages
 
 # Create your views here.
+def signup(request):
+    form = RegisterForm()
+    return render(request, 'all_templates/signup_form.html',{"form":form})
+
+def login(request):
+    return render(request,'all_templates/login.html')
